9bcccb4132108a26ddb8a58630d919395bd2289b
laizhiren <laizhiren@aspirecn.com>
2018-09-06 11:42:34 +0800

    flex布局和百分比布局demo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './assets/styles/styles.scss';
 
 import { sliderInit } from './assets/js/slider';
+import { flexible } from './assets/js/flexible.js';
 
 document.addEventListener('DOMContentLoaded', sliderInit);
