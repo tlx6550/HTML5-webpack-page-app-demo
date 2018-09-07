b69c0b5f2ba4bbf0d88a2a142eceb2694e6f89ad
laizhiren <laizhiren@aspirecn.com>
2018-09-07 13:50:58 +0800

    页面布局检测

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './assets/styles/styles.scss';
 
 import { sliderInit } from './assets/js/slider';
-import { flexible } from './assets/js/flexible.js';
-
+import flexible from './assets/js/flexible.js';
+console.log('flexible='+flexible)
 document.addEventListener('DOMContentLoaded', sliderInit);
