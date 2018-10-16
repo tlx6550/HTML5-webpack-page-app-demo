f64aeffeba1d513ac532ed8a3850ef93939ec213
laizhiren <laizhiren@aspirecn.com>
2018-10-16 14:00:28 +0800

    所有静态页完成

--- src/js/myTuCao.js
+++ src/js/myTuCao.js
@@ -3,7 +3,17 @@
  */
 import '../assets/styles/mmzhuochong.scss';
 import $ from '../assets/js/jquery.min.js';
-
+import  '../assets/js/flexible.js';
 $(function(){
-
+    //只看中奖逻辑
+  $('.check-icon').click(function (e) {
+      e.stopPropagation();
+      $(this).toggleClass('active')
+  })
+    // 筛选
+   $('.btn-group').find('.button').click(function (e) {
+       e.stopPropagation();
+       $(this).siblings().removeClass('active')
+       $(this).toggleClass('active')
+   })
 });
--- src/js/ruleDeail.js
+++ src/js/ruleDeail.js
@@ -2,8 +2,4 @@
  * Created by issuser on 2018/9/28 0028.
  */
 import '../assets/styles/mmzhuochong.scss';
-import $ from '../assets/js/jquery.min.js';
-
-$(function(){
-
-});
+import  '../assets/js/flexible.js';
