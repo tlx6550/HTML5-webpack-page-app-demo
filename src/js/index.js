bbee551d94aebab30aa52a9e4f8b4c6ed0a7558d
laizhiren <laizhiren@aspirecn.com>
2018-10-16 18:34:53 +0800

    个别浏览器不支持copy

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,13 @@
  */
 import '../assets/styles/mmzhuochong.scss';
 import $ from '../assets/js/jquery.min.js';
+import Clipboard from '../assets/js/clipboard.min';
 import '../assets/js/swiper.min.js';
+import VConsole from '../assets/js/vConsole.js';
 import  '../assets/js/flexible.js';
 
 // app download
+var vConsole = new VConsole();
 import '../js/mmdl.js';
 import '../js/mmapp.js';
 import initAllApp from '../js/tab_control.js';
@@ -283,15 +286,354 @@ import initAllApp from '../js/tab_control.js';
     $.fn.tab = Plugin;
 
 }(window);
+!function (window) {
+// 跳转活动规则
+$('.rules-btn').click(function () {
+    location.href = './ruleDeail.html'
+})
 
+// 跳转活动规则
+    $('.wodetucao').click(function () {
+        location.href = './myTuCao.html'
+    })
+// 跳转活动规则
+    $('.tucaozhixin').click(function () {
+        location.href = './monthRane.html'
+ })
+// 跳转活动规则
+    $('.liketucao').click(function () {
+        location.href = './fankuisearch.html'
+    })
+// 跳转活动规则
+    $('.buhaoyong').click(function () {
+        location.href = './fankuiyijian.html'
+    })
+}(window)
+!function (window) {
+    "use strict";
 
-const appName = {
-    '001': 'aiqiyi',
-    '002': 'wangyi',
-    '003': 'toutiao',
-    '004': 'youku'
-}
-let appId = ''
+    var doc = window.document,
+        ydui = {};
+
+    /**
+     * 直接绑定FastClick
+     */
+    $(window).on('load', function () {
+        typeof FastClick == 'function' && FastClick.attach(doc.body);
+    });
+
+    var util = ydui.util = {
+        /**
+         * 格式化参数
+         * @param string
+         */
+        parseOptions: function (string) {
+            if ($.isPlainObject(string)) {
+                return string;
+            }
+
+            var start = (string ? string.indexOf('{') : -1),
+                options = {};
+
+            if (start != -1) {
+                try {
+                    options = (new Function('', 'var json = ' + string.substr(start) + '; return JSON.parse(JSON.stringify(json));'))();
+                } catch (e) {
+                }
+            }
+            return options;
+        },
+        /**
+         * 页面滚动方法【移动端】
+         * @type {{lock, unlock}}
+         * lock：禁止页面滚动, unlock：释放页面滚动
+         */
+        pageScroll: function () {
+            var fn = function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+            };
+            var islock = false;
+
+            return {
+                lock: function () {
+                    if (islock)return;
+                    islock = true;
+                    doc.addEventListener('touchmove', fn);
+                },
+                unlock: function () {
+                    islock = false;
+                    doc.removeEventListener('touchmove', fn);
+                }
+            };
+        }(),
+        /**
+         * 本地存储
+         */
+        localStorage: function () {
+            return storage(window.localStorage);
+        }(),
+        /**
+         * Session存储
+         */
+        sessionStorage: function () {
+            return storage(window.sessionStorage);
+        }(),
+        /**
+         * 序列化
+         * @param value
+         * @returns {string}
+         */
+        serialize: function (value) {
+            if (typeof value === 'string') return value;
+            return JSON.stringify(value);
+        },
+        /**
+         * 反序列化
+         * @param value
+         * @returns {*}
+         */
+        deserialize: function (value) {
+            if (typeof value !== 'string') return undefined;
+            try {
+                return JSON.parse(value);
+            } catch (e) {
+                return value || undefined;
+            }
+        }
+    };
+
+    /**
+     * HTML5存储
+     */
+    function storage (ls) {
+        return {
+            set: function (key, value) {
+                ls.setItem(key, util.serialize(value));
+            },
+            get: function (key) {
+                return util.deserialize(ls.getItem(key));
+            },
+            remove: function (key) {
+                ls.removeItem(key);
+            },
+            clear: function () {
+                ls.clear();
+            }
+        };
+    }
+
+    /**
+     * 判断css3动画是否执行完毕
+     * @git http://blog.alexmaccaw.com/css-transitions
+     * @param duration
+     */
+    $.fn.emulateTransitionEnd = function (duration) {
+        var called = false,
+            $el = this;
+
+        $(this).one('webkitTransitionEnd', function () {
+            called = true;
+        });
+
+        var callback = function () {
+            if (!called) $($el).trigger('webkitTransitionEnd');
+        };
+
+        setTimeout(callback, duration);
+    };
+
+    if (typeof define === 'function') {
+        define(ydui);
+    } else {
+        window.YDUI = ydui;
+    }
+
+}(window);
+/**
+ * Tab Plugin
+ */
+!function (window) {
+    "use strict";
+
+    function Tab (element, options) {
+        this.$element = $(element);
+        this.options = $.extend({}, Tab.DEFAULTS, options || {});
+        this.init();
+        this.bindEvent();
+        this.transitioning = false;
+    }
+
+    // 150ms 为切换动画执行时间
+    Tab.TRANSITION_DURATION = 150;
+
+    Tab.DEFAULTS = {
+        nav: '.tab-nav-item',
+        panel: '.tab-panel-item',
+        activeClass: 'tab-active'
+    };
+
+    Tab.prototype.init = function () {
+        var _this = this,
+            $element = _this.$element;
+
+        _this.$nav = $element.find(_this.options.nav);
+        _this.$panel = $element.find(_this.options.panel);
+    };
+
+    /**
+     * 給选项卡导航绑定点击事件
+     */
+    Tab.prototype.bindEvent = function () {
+        var _this = this;
+        _this.$nav.each(function (e) {
+            $(this).on('click.ydui.tab', function () {
+                _this.open(e);
+            });
+        });
+    };
+
+    /**
+     * 打开选项卡
+     * @param index 当前导航索引
+     */
+    Tab.prototype.open = function (index) {
+        var _this = this;
+
+        index = typeof index == 'number' ? index : _this.$nav.filter(index).index();
+
+        var $curNav = _this.$nav.eq(index);
+
+        // 如果切换动画进行时或者当前二次点击 禁止重复操作
+        if (_this.transitioning || $curNav.hasClass(_this.options.activeClass))return;
+
+        _this.transitioning = true;
+
+        // 打开选项卡时绑定自定义事件
+        $curNav.trigger($.Event('open.ydui.tab', {
+            index: index
+        }));
+
+        // 给tab导航添加选中样式
+        _this.active($curNav, _this.$nav);
+
+        // 给tab内容添加选中样式
+        _this.active(_this.$panel.eq(index), _this.$panel, function () {
+            // 打开选项卡后绑定自定义事件
+            $curNav.trigger({
+                type: 'opened.ydui.tab',
+                index: index
+            });
+            _this.transitioning = false;
+        });
+    };
+
+    /**
+     * 添加选中样式
+     * @param $element 当前需要添加选中样式的对象
+     * @param $container 当前对象的同级所有对象
+     * @param callback 回调
+     */
+    Tab.prototype.active = function ($element, $container, callback) {
+        var _this = this,
+            activeClass = _this.options.activeClass;
+
+        var $avtive = $container.filter('.' + activeClass);
+
+        function next () {
+            typeof callback == 'function' && callback();
+        }
+
+        // 动画执行完毕后回调
+        $element.one('webkitTransitionEnd', next).emulateTransitionEnd(Tab.TRANSITION_DURATION);
+
+        $avtive.removeClass(activeClass);
+        $element.addClass(activeClass);
+    };
+
+    function Plugin (option) {
+        var args = Array.prototype.slice.call(arguments, 1);
+
+        return this.each(function () {
+            var target = this,
+                $this = $(target),
+                tab = $this.data('ydui.tab');
+
+            if (!tab) {
+                $this.data('ydui.tab', (tab = new Tab(target, option)));
+            }
+
+            if (typeof option == 'string') {
+                tab[option] && tab[option].apply(tab, args);
+            }
+        });
+    }
+
+    $(window).on('load.ydui.tab', function () {
+        $('[data-ydui-tab]').each(function () {
+            var $this = $(this);
+            $this.tab(window.YDUI.util.parseOptions($this.data('ydui-tab')));
+        });
+    });
+
+    $.fn.tab = Plugin;
+
+}(window);
+!function (window) {
+    //检查是否支持复制
+    function checkSerportCopy(){
+        $('.copy-btn').click(function (e) {
+            setTimeout(function () {
+               var flag =  YDUI.util.sessionStorage.get('copyFlag')
+              if(!flag){
+                  $('.fail').show()
+                  setTimeout(function () {
+                      $('.fail').hide()
+                  },1000)
+              }
+            },500)
+        })
+    }
+    checkSerportCopy();
+    function showPop(txt){
+        var dfd = $.Deferred();
+        $('.sucess').text(txt).show()
+        setTimeout(function () {
+            $('.toast').hide()
+            dfd.resolve();
+        },1000)
+        return dfd;
+    }
+// 搜索反馈样式拷贝
+  var searchClipboard =   new Clipboard('#search-copy-btn', {
+        text: function(trigger) {
+            return '#吐槽#短视频 + 抖音短视频 + 搜索不到这个应用';
+        }
+    });
+    searchClipboard.on('success', function(e) {
+        YDUI.util.sessionStorage.set('copyFlag',true)
+        showPop('复制成功').then(function () {
+            e.clearSelection();
+            location.href= './fankuisearch.html'
+        })
+    });
+   searchClipboard.on('error', function(e) {
+       showPop('复制失败')
+    });
+    // 搜索应用样式拷贝
+    var appClip = new Clipboard('#app-copy-btn');
+    appClip.on('success', function(e) {
+        YDUI.util.sessionStorage.set('copyFlag',true)
+        showPop('复制成功').then(function () {
+            e.clearSelection();
+            location.href= './fankuiyijian.html'
+        })
+
+    });
+    appClip.on('error', function(e) {
+        showPop('复制失败')
+    });
+}(window)
 
 
 
